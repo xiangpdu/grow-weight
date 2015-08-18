33de307f7eee72c12eea9829b4e5877e0897ea35
duxiangping <xpdu@duxiangpingdeMacBook-Pro.local>
2015-08-18 18:12:16 +0800

    初始工程

---
+++ grow-weight/grow-weight/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  grow-weight
+//
+//  Created by duxiangping on 15/8/18.
+//  Copyright (c) 2015年 duxiangping. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
